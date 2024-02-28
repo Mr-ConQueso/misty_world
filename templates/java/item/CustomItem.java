0bed4e7d28152b8a7dac59ecd9045f0002581cc1
Mr-ConQueso <nachocolorado04@gmail.com>
2024-02-28 12:33:39 +0100

    another clean up

--- templates/java/item/CustomItem.java
+++
@@ -1,4 +0,0 @@
-+++ item/ModItems.java
-@@@ after:@TemplateEntryPoint("Register Items") @@@
-  public static final RegistryObject<Item> [[ --ITEM_NAME-- ]] = ITEMS.register([[ --ItemClassName-- ]].NAME,
-      () -> new [[ --ItemClassName-- ]](new Item.Properties().tab(CreativeModeTab.[[ --CreativeTab-- ]])));
--- templates/java/item/SimpleItem.java
+++
@@ -1,4 +0,0 @@
-+++ item/ModItems.java
-@@@ after:@TemplateEntryPoint("Register Items") @@@
-  public static final RegistryObject<Item> [[ --ITEM_NAME-- ]] = ITEMS.register("[[ --item_name-- ]]",
-      () -> new Item(new Item.Properties().tab(CreativeModeTab.[[ --CreativeTab-- ]])));
